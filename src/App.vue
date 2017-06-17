aca6595eb266833a732abe3c6ea095cc8bc1ece6
lwio <443474713@qq.com>
2017-06-17 23:01:16 +0800

    initial

---
+++ src/App.vue
@@ -0,0 +1,96 @@
+<template>
+  <div id="app">
+    <router-view name="header"></router-view>
+    <transition name="fade" mode="out-in">
+      <router-view></router-view>
+    </transition>
+    <music-player></music-player>
+    <!--音频播放标签-->
+    <audio src="" ref="audio" autoplay="autoplay" @ended="" @timeupdate="syncCurrentTime" @canplay=""></audio>
+  </div>
+</template>
+
+<script>
+import MusicPlayer from '@/components/MusicPlayer'
+import {mapActions,mapGetters} from 'vuex'//使用状态管理里的mapActions来传递方法，mapGetters来展示数据
+export default {
+  name: 'app',
+  components:{
+    MusicPlayer
+  },
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+    ...mapActions([
+        'setPlayerDOM',
+        'syncCurrentTime',//循环播放（自动下一曲）
+    ]),
+    change() {
+      this.dialogTableVisible = true
+      console.log("fuck")
+    }
+  },
+  mounted() {
+    this.$store.dispatch('setPlayerDOM',this.$refs.audio);
+  },
+
+}
+</script>
+
+<style lang="scss">
+body, h1, h2, h3, h4, h5, h6, hr, p, blockquote, dl, dt, dd, ul, ol, li, pre, form, fieldset, legend, button, input, textarea, th, td { margin:0; padding:0; } 
+body, button, input, select, textarea { font:12px/1.5tahoma, arial, \5b8b\4f53; } 
+h1, h2, h3, h4, h5, h6{ font-size:100%; } 
+address, cite, dfn, em, var { font-style:normal; } 
+code, kbd, pre, samp { font-family:couriernew, courier, monospace; } 
+small{ font-size:12px; } 
+ul, ol { list-style:none; } 
+a { text-decoration:none; } 
+a:hover { text-decoration:underline; } 
+sup { vertical-align:text-top; } 
+sub{ vertical-align:text-bottom; } 
+legend { color:#000; } 
+fieldset, img { border:0; } 
+button, input, select, textarea { font-size:100%; } 
+table { border-collapse:collapse; border-spacing:0; }
+
+button {
+  &:focus {
+    outline: none;
+  }
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.divider {
+  width: 80%;
+  margin:0 auto;
+  border-bottom: 1px solid black;
+}
+.hjbook-tester {
+    background-color: #58b7ff;
+    position: fixed;
+    right: 100px;
+    bottom: 150px;
+    width: 50px;
+    height: 50px;
+    border-radius: 25px;
+    cursor: pointer;
+    opacity: .4;
+}
+a {
+  color: inherit;
+  text-decoration: none;
+}
+ul, ol {
+  list-style-type: none;
+}
+</style>
